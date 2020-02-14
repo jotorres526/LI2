e91c6210a503c81d21ba8777b2afc3d27172afec
jotorres526 <jotorres526@gmail.com>
2020-02-14 11:26:16 +0000

    Changed float's to int's

--- ficha1/triangles.c
+++ ficha1/triangles.c
@@ -1,10 +1,10 @@
 #include <stdlib.h>
 #include <math.h>
 #include <stdio.h>
-//Receives 3 float's 
+//Receives 3 int's 
 //Returns 0 in case they do not form a triangle (not valid)
 //Returns 1 in case they form triangle (valid)
-int valid(float a, float b, float c) {
+int valid(int a, int b, int c) {
 	int r;
 	r = 0;
 	if(a > abs(b - c) && a < b + c) {
@@ -17,27 +17,24 @@ int valid(float a, float b, float c) {
 	return r;
 }
 
-//Receives 3 float's that form a triangle 
-//If all 3 float's are equal it will print Equilateral
-//If all 2 float's are equal and 1 is different it will print Isosceles
-//If all 3 float's are different it will print Scalene
-void type(float a, float b, float c) {
+//Receives 3 int's that form a triangle and will print it's type accordingly
+void type(int a, int b, int c) {
 	printf("Triangle type: ");
 	if (a == b && a == c) {
 		printf ("Equilateral\n"); 
-	} else if(a == b || a == c) { 
+	} else if(a == b || a == c || b == c) { 
 		printf ("Isosceles\n");
 	} else {
 		printf ("Scalene\n");
 	}
 }
 
-//Receives 3 float's that form a triangle
-//Calculates the area of said triangle using Heron's formula
-float area(float a, float b, float c) {
+//Receives 3 int's that form a triangle
+//Calculates it's area using Heron's formula
+float area(int a, int b, int c) {
 	float s;
 	float r; 
-	s = (a + b + c)/2;
+	s = (a + b + c)/2.0f;
 	r = sqrt(s * (s - a) * (s - b) * (s - c));
 	printf("This triangle has an area of: %f\n", r);
 	return r;
@@ -45,10 +42,10 @@ float area(float a, float b, float c) {
 
 //main
 int main() {
-	float a, b, c;
+	int a, b, c;
 	a = b = c = 0;
 	printf("Input 3 values: \n");
-	scanf("%f %f %f", &a, &b, &c);
+	scanf("%d %d %d", &a, &b, &c);
 	if (valid(a, b, c) == 0) {
 		printf("Invalid triangle\n");
 		return -1;
