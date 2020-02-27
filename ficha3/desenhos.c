ca1976e12123e7b6c7be8fadc51ba0ab3fa09354
jotorres526 <jotorres526@gmail.com>
2020-02-27 22:25:16 +0000

    - Changed some comments

--- ficha3/desenhos.c
+++ ficha3/desenhos.c
@@ -10,7 +10,7 @@ void drawSpaces(int height, int line) {
     for(int i = 0; i < spaces; i++) putchar(' ');
 }
 
-//Calculates a line's character sequence
+//Calculates a line's sequence
 //Based on max height and current line
 //Prints said char sequence
 //Ex: 'A' 'B' 'C' 'B' 'A'
@@ -30,6 +30,7 @@ void drawSeq(int height, int line) {
 
 
 //Prints a sequence of characters forming a triangle
+//height needs to be greater than 1
 void imprime_triangulo(int height) {
     if(height < 2) {
         printf("Um triangulo necessita de uma altura maior que 1.\n");
@@ -43,12 +44,11 @@ void imprime_triangulo(int height) {
 }
 
 //Prints a sequence of characters forming a diamond
-//Starts out by calculating the "fstHalf" and prints a triangle based on its height
-//Then, prints an inverted triangle based on the "sndHalf" 
-//fstHalf >= sndHalf
+//Starts out by printing a triangle based on given height
+//Then, prints an inverted triangle with the same height, removing the duplicated middle sequence
+//height needs to be greater than 1
 void imprime_losango(int height) {
     int sndHalf;
-    
     if(height < 2) {
         printf("Um losango necessita de uma altura maior que 1.\n");
         return;
@@ -56,21 +56,20 @@ void imprime_losango(int height) {
     
     sndHalf = height - 1;
     imprime_triangulo(height);
-    //Starts the loop on the sndHalf but still prints spaces based on the first half
-    //so the middle line doesn't print twice, but the spaces display correctly
-    for(int i = sndHalf; i != 0; i--) {
-        drawSpaces(height, i);
+    for(int i = sndHalf; i > 0; i--) {
+        drawSpaces(height, i); //The spaces are calculated based on the total height
         drawSeq(sndHalf, i);
         putchar('\n');
     }
 }
 
+
 //Receives an hexagon line and height
-//Prints a line of that hexagon
+//Calculates the shape of that line and prints it
 void drawHexLine(int height, int line) {
     int nspaces, ntags;
-    nspaces = height - line;
-    ntags = height + 2 * (line - 1);
+    nspaces = height - line; //Last line has 0 spaces
+    ntags = height + 2 * (line - 1); //number of # starts out equal to height in the first line and for each other line adds 2 #
     for(int j = 0; j < nspaces; j++) putchar(' ');
     for(int j = 0; j < ntags; j++) {
         if (j == 0 || j == (ntags - 1) || line == 1) putchar('#');
@@ -80,17 +79,16 @@ void drawHexLine(int height, int line) {
 }
 
 //Prints a sequence of characters forming a hexagon
-//Starts out by calculating the "fstHalf" and prints the top part of the shape
-//Then, prints the bottom part based on the "sndHalf" 
-//fstHalf >= sndHalf
+//Starts out by printing the upper half
+//Then, prints the bottom part based on the 'sndHalf'
+//height needs to be greater than 1
 void imprime_hexagono(int height){
     int sndHalf;
-    if(height < 3) {
-        printf("Um hexagono necessita de uma altura maior que 2.\n");
+    if(height < 2) {
+        printf("Um hexagono necessita de uma altura maior que 1.\n");
         return;
     }
-    sndHalf = height - 1;
-     //4 + 2(lines - 1) (=) 4 + 2lines - 2 (=) 2 + 2lines
+    sndHalf = height - 1; //- 1 to not duplicate middle line
     for(int line = 1; line <= height; line++) {
         drawHexLine(height, line);
     }
